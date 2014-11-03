25842628f74bbc37a9b36fe284fa107459075aba
AA3236 <AA3236@PC-00105180.uni.mdx.ac.uk>
2014-11-03 13:40:02 +0000

    Commit message

---
+++ src/testStudent.java
@@ -0,0 +1,17 @@
+
+public class testStudent {
+	
+	public static void main(String[] args) {
+		
+		Student firstStudent = new Student("John", 19);
+		firstStudent.displayStudentDetails();
+		 }
+		}
+
+public void displayStudentDetails() {
+ System.out.println("Name: " + name + " Age: " + age);
+ }
+ }
+
+
+ 
